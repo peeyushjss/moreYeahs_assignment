af1581092dee4ecb6924114a81b2285a06c6c455
Peeyush Kumar <Peeyush.Kumar@igglobal.com>
2020-03-09 00:02:03 +0530

    POST availability API completed.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,33 @@ const router = express.Router();
 
 /* POST API for storing the availability of User. */
 router.post('/availability', function (req, res, next) {
+    const availabilityDB = req.availability;
+    let startTime = req.body.startTime;
+    let endTime = req.body.endTime;
+    let userId = req.body.userId;
+
+    if (startTime && endTime && userId) {
+        let availabilityData = new availabilityDB({
+            startTime: startTime,
+            endTime: endTime,
+            userId: userId
+        });
+        availabilityData.save(function (err, result) {
+            if (err) {
+                req.json_op_status = 0;
+                req.json_op_message = err;
+                next();
+            } else {
+                req.json_op_status = 1;
+                req.json_op_message = 'Success!';
+                next();
+            }
+        });
+    } else {
+        req.json_op_status = 0;
+        req.json_op_message = 'Fill all fields!';
+        next();
+    }
 
 });
 
